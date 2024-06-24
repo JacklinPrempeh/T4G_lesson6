660deb811e4c979b27355d7a49aaf989d9d49943
JacklinPrempeh <JackieP6132@gmail.com>
2024-06-24 12:39:20 +0000

    addind all files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// alert('Hi my name is Jacklin');
+// let name=prompt('What is your name?');
+// let hometown='ashanti';
+// let age='18';
+// // alert(name);
+// // alert('hello '+ name +' welcome to my website');
+// alert(`hi${name} ,from${hometown},who is ${age},welcome to my page`)
+let title=prompt('what is your title');
+let name=prompt('what is your name');
+let product=prompt('what is your preferred product');
+alert(`Hi ${title} ${name},do you like ${product}?`);
